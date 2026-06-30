37f246da11c120153c5c0ef3193d1a5e035aad64
Abhinav13112 <abhigoud161@gmail.com>
2026-06-30 15:32:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 # ---------------- PAGE CONFIG ----------------
 st.set_page_config(
     page_title="LogSentrix AI",
-    page_icon="assets/LogSentrixAi logo.png",
+    page_icon="asserts/logo.png",
     layout="centered"
 )
 
@@ -49,7 +49,7 @@ st.markdown("""
 
 # ---------------- LOGO ----------------
 st.markdown('<div class="logo">', unsafe_allow_html=True)
-st.image("assets/LogSentrixAi logo.png", width=180)
+st.image("asserts/logo.png", width=180)
 st.markdown("</div>", unsafe_allow_html=True)
 
 # ---------------- TITLE ----------------
