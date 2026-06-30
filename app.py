fc2c144c32214b200d325853d4aa073abe5344f5
Abhinav13112 <abhigoud161@gmail.com>
2026-06-30 15:22:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 import streamlit as st
 
-st.title("LogSentrix AI")
-st.write("Transforming System Logs into Actionable Intelligence..")
+st.set_page_config(page_title="LogSentrix AI", layout="centered")
 
-uploaded_file = st.file_uploader("Upload a log file")
+col1, col2 = st.columns([1,4])
+
+with col1:
+    st.image("LogSentrixAi logo.png", width=80)
+
+with col2:
+    st.title("LogSentrix AI")
+    st.caption("Transforming System Logs into Actionable Intelligence.")
+
+uploaded_file = st.file_uploader(
+    "Upload a Log File",
+    type=["csv", "log", "txt"]
+)
 
 if uploaded_file:
-    st.success("File uploaded successfully!")
-    # Call your prediction function here
+    st.success("✅ File uploaded successfully!")
